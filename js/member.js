c7e906c8a8d575b54b6a026526c78d8ccad76d81
zhangxu <zx222@qq.com>
2020-12-05 23:54:20 +0800

    zx

---
+++ js/member.js
@@ -0,0 +1,50 @@
+var member = [
+  {
+    "phone": "",
+    "name": "Sid"
+  },
+  {
+    "phone": "",
+    "name": "Rico"
+  },
+  {
+    "phone": "",
+    "name": "Judy"
+  },
+  {
+    "phone": "",
+    "name": "Alex"
+  },
+  {
+    "phone": "",
+    "name": "Theo"
+  },
+  {
+    "phone": "",
+    "name": "Nicole"
+  },
+  {
+    "phone": "",
+    "name": "Sophia"
+  },
+  {
+    "phone": "",
+    "name": "Olivia"
+  },
+  {
+    "phone": "",
+    "name": "Cher"
+  },
+  {
+    "phone": "",
+    "name": "Devin"
+  },
+  {
+    "phone": "",
+    "name": "Rena"
+  },
+  {
+    "phone": "",
+    "name": "Gavin"
+  },
+]
